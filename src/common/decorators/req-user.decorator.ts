6ec6f38512a7fbea534f62998d39d539605e82e4
sajjadmrx <sajjadzibaee102@gmail.com>
2022-03-02 14:33:19 +0330

    object

--- src/common/decorators/req-user.decorator.ts
+++ src/common/decorators/req-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 
-export const getUser = createParamDecorator(<T>(data: string, ctx: ExecutionContext): T => {
+export const getUser = createParamDecorator(<T extends object>(data: string, ctx: ExecutionContext): T => {
   const request = ctx.switchToHttp().getRequest<Request>();
   const user = request['user']
 
@@ -10,6 +10,3 @@ export const getUser = createParamDecorator(<T>(data: string, ctx: ExecutionCont
 })
 
 
-// class User {}
-
-// getUser<User>('id')
