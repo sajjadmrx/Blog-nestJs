917633c2870ebd09e7e0f783b3f47faa5080e317
sajjadmrx <sajjadzibaee102@gmail.com>
2022-03-03 17:57:39 +0330

    fix the Genric Type

---
+++ src/modules/user/user.repository.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common'
+import { UserEntity } from './model/user.entity';
+
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
+import { IRepository } from 'src/common/interfaces/repository.interface';
+import { IUser } from 'src/common/interfaces/user.interface';
+
+@Injectable()
+export class UserRepository implements IRepository<IUser>{
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>
+  ) { }
+
+
+  async find(page: number = 1, limit: number = 10): Promise<IUser[]> {
+    return this.repository.find({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+  }
+
+  async findById(id: number): Promise<IUser> {
+    return this.repository.findOne({ id })
+  }
+
+  async findOneByEmailAndUsername(email: string, username: string): Promise<IUser> {
+    return this.repository.findOne({ email, username })
+  }
+
+  async findOneByUsername(username: string): Promise<IUser> {
+    return this.repository.findOne({ username })
+  }
+
+  async findOneByEmail(email: string): Promise<IUser> {
+    return this.repository.findOne({ email })
+  }
+
+  async findByEmailOrUsername(email: string, username: string): Promise<IUser[]> {
+    return this.repository.find({
+      where: [
+        { email },
+        { username },
+      ]
+    })
+  }
+
+  async create(user: IUser): Promise<IUser> {
+    return this.repository.save(user)
+  }
+
+
+  // async delete(id: number): Promise<void> {
+  //   return this.repository.delete(id)
+  // }
+
+  async update(id: number, entity: IUser): Promise<IUser> {
+    return entity
+  }
+}
+
