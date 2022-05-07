0da11e49ca566d2e2232e1c089dbae00582633d3
mrx <sajjadzibaee102@gmail.com>
2022-05-07 10:20:34 +0430

    auth

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -5,6 +5,7 @@ import { ApiBearerAuth, ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { diskStorage } from "multer";
 import { getResponseMessage } from "src/common/constants/messages.constant";
 import { ApiFile } from "src/common/decorators/api-File.decorator";
+import CheckRoleGuard from "src/common/guards/check-roles.guard";
 import { postFilter } from "./filters/post.filter";
 import { uploadService } from "./upload.service";
 import { postStorage } from "./upload.storages";
@@ -12,8 +13,9 @@ import { postStorage } from "./upload.storages";
 
 @Controller('uploads')
 @ApiTags("Upload File")
-// @UseGuards(AuthGuard("jwt"))
-// @ApiBearerAuth()
+@UseGuards(CheckRoleGuard(["ADMIN"]))
+@UseGuards(AuthGuard("jwt"))
+@ApiBearerAuth()
 export class UploadController {
 
     constructor(private uploadService: uploadService) { }
