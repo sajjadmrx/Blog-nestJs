a7272d7c4c3d5876d62c65230044d6fe54fe90f3
mrx <sajjadzibaee102@gmail.com>
2022-05-22 18:48:03 +0430

    added static assets

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 (async () => {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix("api/v1");
-
+  app.useStaticAssets('uploads', {
+    prefix: '/uploads/'
+  })
   const config = new DocumentBuilder()
     .setTitle('Blog - NestJS')
     .setDescription('The Blog API description')
@@ -24,7 +27,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-
+  //public to upload filder
   await app.listen(port)
   console.log(`Server running on ${port}`)
 })()
