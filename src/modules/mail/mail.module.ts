baca60fc0351dd5e330df495850c73f24a23a7aa
sajjadmrx <sajjadzibaee102@gmail.com>
2022-08-12 12:58:01 +0430

    update Mailer config

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -3,24 +3,28 @@ import { MailerModule } from "@nestjs-modules/mailer";
 import { EjsAdapter } from "@nestjs-modules/mailer/dist/adapters/ejs.adapter";
 import { MailService } from "./mail.service";
 import path, { join } from "path";
+import { ConfigService } from "@nestjs/config";
 
 const providersAndExports = [MailService];
 @Global()
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
+    MailerModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>("EMAIL_HOST"),
+          port: config.get<number>("EMAIL_PORT"),
+          auth: {
+            user: config.get<string>("EMAIL_USER"),
+            pass: config.get<string>("EMAIL_PASS"),
+          },
         },
-      },
-      template: {
-        dir: join(path.resolve(), "templates"),
-        adapter: new EjsAdapter(),
-      },
+        template: {
+          dir: join(path.resolve(), "templates"),
+          adapter: new EjsAdapter(),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
