b9803c6f238cbd8c8c67758c7253952d065fa038
sajjadmrx <sajjadzibaee102@gmail.com>
2022-08-25 16:50:49 +0430

    add env ex

---
+++ .env.ex
@@ -0,0 +1,5 @@
+EMAIL_HOST= # you can use https://mailtrap.io/
+EMAIL_PORT= # you can use https://mailtrap.io/
+EMAIL_USER= # you can use https://mailtrap.io/
+EMAIL_PASS= # you can use https://mailtrap.io/
+APP_MODE=development #or production
