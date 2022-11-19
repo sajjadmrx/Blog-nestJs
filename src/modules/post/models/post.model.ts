a68effd24349677546b6cc3b8a6141bda8723d78
sajjadmrx <sajjadzibaee102@gmail.com>
2022-11-19 16:34:12 +0330

    graphql: get all posts

---
+++ src/modules/post/models/post.model.ts
@@ -0,0 +1,26 @@
+import { Field, ID, Int, ObjectType } from "@nestjs/graphql";
+import { Post } from "../../../shared/interfaces/post.interface";
+
+@ObjectType({
+  description: "Post Model",
+})
+export class PostModel implements Post {
+  @Field((type) => ID)
+  id: number;
+  @Field((type) => String)
+  title: string;
+  @Field((type) => String)
+  content: string;
+  @Field((type) => Int)
+  authorId: number;
+  @Field((type) => Boolean)
+  published: boolean;
+  @Field((type) => String)
+  cover: string;
+  @Field((type) => Date)
+  createdAt: Date;
+  @Field((type) => Date)
+  updatedAt: Date;
+  @Field((type) => String)
+  tags: string;
+}
--- src/modules/post/post.resolver.ts
+++ src/modules/post/post.resolver.ts
@@ -1,11 +1,16 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
+import { PostModel } from "./models/post.model";
+import { PostRepository } from "./post.repository";
 
 @Resolver()
 export class PostResolver {
-  constructor() {}
+  constructor(private postRepository: PostRepository) {}
 
-  @Query(() => String)
-  sayHello(): string {
-    return "Hello World!";
+  @Query((returns) => [PostModel])
+  async all(
+    @Args("page", { nullable: false }) page: number,
+    @Args("limit", { nullable: false }) limit: number
+  ): Promise<Array<PostModel>> {
+    return this.postRepository.findPublic(page, limit, {});
   }
 }
