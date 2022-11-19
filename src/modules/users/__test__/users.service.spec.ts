f532cd4721bc1d6b1664ce58676261d72db460be
sajjadmrx <sajjadzibaee102@gmail.com>
2022-11-19 16:15:32 +0330

    clean

--- src/modules/users/__test__/users.service.spec.ts
+++ src/modules/users/__test__/users.service.spec.ts
@@ -3,14 +3,16 @@ import { UsersRepository } from "../users.repository";
 import { User } from "../../../shared/interfaces/user.interface";
 import { BadRequestException } from "@nestjs/common";
 import { RoleType } from "../../../shared/interfaces/role.interface";
-const user: User = {
-  username: "mrx",
-  email: "mrx@gmail.com",
-  role: "ADMIN",
-  id: 1,
-  password: "hash",
-  createdAt: new Date(),
-  updatedAt: new Date(),
+const getFakeUser = (): User => {
+  return {
+    username: "mrx",
+    email: "mrx@gmail.com",
+    role: "ADMIN",
+    id: 1,
+    password: "hash",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
 };
 describe("UserService", function () {
   let usersService: UsersService;
@@ -22,24 +24,28 @@ describe("UserService", function () {
 
   describe("getProfile()", function () {
     it("should return user without password field", () => {
+      const user: User = getFakeUser();
       const result = usersService.getProfile(user);
-      delete user.password;
 
       expect(result).toEqual(user);
     });
     it("should password is undefined", () => {
+      const user: User = getFakeUser();
       const result = usersService.getProfile(user);
+
       expect(result.password).toBeUndefined();
     });
   });
   describe("updateRole()", function () {
     it("should reject 'INVALID_ROLE' when set unknown role", () => {
+      const user: User = getFakeUser();
       const role = "MANAGER";
       expect(usersService.updateRole(user.id, role)).rejects.toEqual(
         new BadRequestException("INVALID_ROLE")
       );
     });
     it("should reject 'INVALID_USER_ID' when set unknown userId", async () => {
+      const user: User = getFakeUser();
       jest.spyOn(usersRepository, "update").mockImplementation(() => {
         throw new Error("invalid"); //database error
       });
@@ -49,6 +55,7 @@ describe("UserService", function () {
       );
     });
     it("should update user role and return role", async () => {
+      const user: User = getFakeUser();
       const role: RoleType = "ADMIN";
       jest
         .spyOn(usersRepository, "update")
