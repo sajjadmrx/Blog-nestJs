d63386780224d79cd3eb5cfac12a81b8eac26186
sajjad isvand <sajjadzibaee102@gmail.com>
2023-08-15 11:12:28 +0330

    save url to db & refactoring

--- main-service/src/app.module.ts
+++ main-service/src/app.module.ts
@@ -4,7 +4,7 @@ import { AuthModule } from "./modules/auth/auth.module";
 import { CategoriesModule } from "./modules/categories/categories.module";
 import { PostModule } from "./modules/post/post.module";
 import { PrismaModule } from "./modules/prisma/prisma.module";
-import { UploadModule } from "./modules/upload/upload.module";
+import { UploadModule } from "./modules/upload/upload.module"; 
 import { UserModule } from "./modules/users/users.module";
 import { MailModule } from "./modules/mail/mail.module";
 import { ConfigModule, ConfigService } from "@nestjs/config";
